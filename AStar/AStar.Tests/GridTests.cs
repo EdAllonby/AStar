efc4d7b8dcaf2f99a4e4019afda5b7a93faf96e4
Ed Allonby <edward.allonby@gmail.com>
2015-06-20 08:35:03 +0100

    Added more heuristic tests.

--- AStar/AStar.Tests/GridTests.cs
+++ AStar/AStar.Tests/GridTests.cs
@@ -8,6 +8,47 @@ namespace AStar.Tests
     public class GridTests
     {
         [Test]
+        public void CalculateHeuristics_WhenANodeIsTwoNodesLeftOfTarget_UsingManhattan()
+        {
+            Node endNode = new Node(new Coordinate(3, 2));
+            Node startNode = new Node(new Coordinate(1, 2));
+
+            List<Node> nodes = new List<Node>
+            {
+                new Node(new Coordinate(1, 1)),
+                startNode,
+                new Node(new Coordinate(2, 1)),
+                new Node(new Coordinate(2, 2)),
+                new Node(new Coordinate(3, 1)),
+                endNode
+            };
+            Grid grid = new Grid(nodes) { EndNode = endNode };
+            grid.CalculateHeuristics(new ManhattanCalculator());
+
+            Assert.AreEqual(startNode.Heuristic, 2);
+        }
+
+        [Test]
+        public void CalculateHeuristics_EndNodeShouldHaveAHeuristicOf0_UsingManhattan()
+        {
+            Node endNode = new Node(new Coordinate(2, 2));
+
+            List<Node> nodes = new List<Node>
+            {
+                new Node(new Coordinate(1, 1)),
+                new Node(new Coordinate(1, 2)),
+                new Node(new Coordinate(2, 1)),
+                endNode
+            };
+
+            Grid grid = new Grid(nodes) { EndNode = endNode };
+
+            grid.CalculateHeuristics(new ManhattanCalculator());
+
+            Assert.AreEqual(endNode.Heuristic, 0);
+        }
+
+        [Test]
         public void CalculateHeuristics_WithA2By2Grid_UsingManhattan()
         {
             Node endNode = new Node(new Coordinate(2, 2));
@@ -21,7 +62,6 @@ namespace AStar.Tests
             };
 
             Grid grid = new Grid(nodes) {EndNode = endNode};
-
 
             grid.CalculateHeuristics(new ManhattanCalculator());
 
